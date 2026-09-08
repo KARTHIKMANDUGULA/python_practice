b1dde33b72028c109462219a1609fafc76fa13e6
KARTHIK MANDUGULA <karthikmandugula13@gmail.com>
2026-09-08 19:48:15 +0530

    added dual_converter_for_ascii

--- ascii_converter.py
+++ ascii_converter.py
@@ -1,4 +1,11 @@
-word="A"
-ascii_value = ord(word)
-print ("ascii value of A is :",ascii_value)
+TYPE=input("ARE YOU GIVING NUMBER OR WORD TO GET ASCII VALUE:")
 
+if TYPE=="NUMBER":
+    NUMBER=int(input("ENTER THE NUMBER TO GET ASCII VALUE IN WORD:"))
+    ascii_value_NUMBER=chr(NUMBER)
+    print(f"THE ASCII VALUE OF {NUMBER} IS:",ascii_value_NUMBER)
+
+elif TYPE=="WORD":
+    WORD = input("ENTER THE WORD TO GET ASCII VALUE IN NUMBER:")
+    ascii_value_WORD =ord(WORD)
+    print(f"THE ASCII VALUE OF {WORD} IS:",ascii_value_WORD)
