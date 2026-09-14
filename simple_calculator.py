360142c416553946e11be244ea134d9deceb3dfc
KARTHIK MANDUGULA <karthikmandugula13@gmail.com>
2026-09-14 12:51:29 +0530

    added_simple_calculator

---
+++ simple_calculator.py
@@ -0,0 +1,12 @@
+num1=int(input("give num1 value:"))
+num2=int(input("give num2 value:"))
+operation=input("give the type of operation:")
+if operation=="+":
+    print("the sum of numbers is:",num1+num2)
+elif operation=="-":
+    print("the subtraction is:",num1-num2)
+elif operation=="*":
+    print("the multiplication is:",num1*num2)
+elif operation=="/":
+    print("the division is:",num1/num2)
+
